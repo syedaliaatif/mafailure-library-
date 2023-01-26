a4c11a8f9aac0113d3137170e9fd4cd6098a97de
Syed Ali Aatif <aatifofficial@gmail.com>
2023-01-27 01:11:30 +0530

    Added Line Container

---
+++ Line Container/solve.h
@@ -0,0 +1,47 @@
+/* 
+################NOT WRITTEN BY ME#################
+Line Container Source :Simon Lindholm 
+
+This can be used to exploit convex hull trick
+to find the max value at certain point. 
+
+Add: Add a new line to a multiset of line 
+Query: return the max(k_i*x+m_i) 
+
+If you want to find the minimum value 
+Insert the negatives of line and -Query(x) will be your required answer 
+
+*/
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long 
+struct Line {
+	mutable ll k, m, p;
+	bool operator<(const Line& o) const { return k < o.k; }
+	bool operator<(ll x) const { return p < x; }
+};
+
+struct LineContainer : multiset<Line, less<>> {
+	// (for doubles, use inf = 1/.0, div(a,b) = a/b)
+	static const ll inf = LLONG_MAX;
+	ll div(ll a, ll b) { // floored division
+		return a / b - ((a ^ b) < 0 && a % b); }
+	bool isect(iterator x, iterator y) {
+		if (y == end()) return x->p = inf, 0;
+		if (x->k == y->k) x->p = x->m > y->m ? inf : -inf;
+		else x->p = div(y->m - x->m, x->k - y->k);
+		return x->p >= y->p;
+	}
+	void add(ll k, ll m) {
+		auto z = insert({k, m, 0}), y = z++, x = y;
+		while (isect(y, z)) z = erase(z);
+		if (x != begin() && isect(--x, y)) isect(x, y = erase(y));
+		while ((y = x) != begin() && (--x)->p >= y->p)
+			isect(x, erase(y));
+	}
+	ll query(ll x) {
+		assert(!empty());
+		auto l = *lower_bound(x);
+		return l.k * x + l.m;
+	}
+};
